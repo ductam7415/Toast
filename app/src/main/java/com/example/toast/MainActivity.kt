16675717c6ded3fc46fa3e9d5baaac8fce8dbe68
dailyhomework29-4 <tamnguyenduc@gmail.com>
2021-06-02 22:10:12 +0700

    bai tap 1/6

--- app/src/main/java/com/example/toast/MainActivity.kt
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.toast
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Toast.makeText(applicationContext, "Chào mừng bạn đã trở lại", Toast.LENGTH_SHORT).show()
+        fun viewNoteDetail() {
+            val intent: Intent = Intent(this, SecondActivity::class.java)
+        }
+        val editText = findViewById<EditText>(R.id.ten)
+        val button = findViewById<Button>(R.id.nextview)
+        button.setOnClickListener(){
+            val intent: Intent = Intent(this, SecondActivity::class.java)
+            val name = editText.text.toString()
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
+            intent.putExtra("tro lai", name)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/toast/SecondActivity.kt
@@ -0,0 +1,26 @@
+package com.example.toast
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val bundle:Bundle? = intent.extras
+        val msg = bundle!!.getString("tro lai")
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        val ten = findViewById<TextView>(R.id.newview)
+        val button = findViewById<Button>(R.id.back)
+        ten.text="hello "+msg
+        button.setOnClickListener(){
+            val intent:Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        }
+    }
