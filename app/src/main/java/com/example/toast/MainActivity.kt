612a6bf4422469c8c5aec2516417beb050e64a4d
dailyhomework29-4 <tamnguyenduc@gmail.com>
2021-06-09 15:48:57 +0700

    bai tap 8/6

--- app/src/main/java/com/example/toast/MainActivity.kt
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -6,19 +6,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.toast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Toast.makeText(applicationContext, "Chào mừng bạn đã trở lại", Toast.LENGTH_SHORT).show()
+
         fun viewNoteDetail() {
             val intent: Intent = Intent(this, SecondActivity::class.java)
         }
+
         val editText = findViewById<EditText>(R.id.ten)
         val button = findViewById<Button>(R.id.nextview)
-        button.setOnClickListener(){
+
+        button.setOnClickListener() {
             val intent: Intent = Intent(this, SecondActivity::class.java)
             val name = editText.text.toString()
             Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/toast/SecondActivity.kt
+++ app/src/main/java/com/example/toast/SecondActivity.kt
@@ -6,21 +6,26 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.toast.databinding.ActivityMainBinding
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        val bundle:Bundle? = intent.extras
+
+        val bundle: Bundle? = intent.extras
         val msg = bundle!!.getString("tro lai")
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
         val ten = findViewById<TextView>(R.id.newview)
         val button = findViewById<Button>(R.id.back)
-        ten.text="hello "+msg
-        button.setOnClickListener(){
-            val intent:Intent = Intent(this, MainActivity::class.java)
+
+        ten.text = "hello " + msg
+        button.setOnClickListener() {
+            val intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
-        }
     }
+}
